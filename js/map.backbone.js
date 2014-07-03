293e059f224000682a0d680b480ddaf7829041b3
Ian Rees <ian@ianrees.net>
2014-07-02 23:00:47 -0700

    Adding route groups

--- js/map.backbone.js
+++ js/map.backbone.js
@@ -1,5 +1,19 @@
+function seconds_to_clock(t) {
+  function pad(a,b){return([1e15]+a).slice(-b)}
+  var h = Math.floor(t/3600);
+  var m = Math.floor((t%3600)/60);
+  return pad(h,2)+':'+pad(m,2)
+}
+
+function get_qs(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 var Route = Backbone.Model.extend({
-  // A single Route.
+  // A single Route variant. e.g. inbound, outbound, special service.
   defaults: {
     display: true,
     los: 0
@@ -7,8 +21,13 @@ var Route = Backbone.Model.extend({
 });
 
 var RouteCollection = Backbone.Collection.extend({
-  // Collection of Routes.
-  url: 'data/actransit.geojson',
+  // All trip variants that belong to an Agency route.
+  model: Route
+})
+
+var AgencyCollection = Backbone.Collection.extend({
+  // All routes belonging to an Agency.
+  url: 'data/test.geojson',
   model: Route,
   parse: function(response) {
     return response.features;
@@ -18,7 +37,7 @@ var RouteCollection = Backbone.Collection.extend({
 var RouteView = Backbone.View.extend({
   // Route list view.
   tagName: "li",
-  template: _.template('<input type="checkbox" class="toggle" checked="checked" /><span><%- properties.route_headsign %></span><button class="only">Show</button>'),
+  template: _.template('<input type="checkbox" class="toggle" checked="checked" /><span><%- properties.route_headsign %></span><button class="only cityism-transvisor-hover">Show</button>'),
   events: {
     "click .only"   : "only",
     "click .toggle"     : "toggle",
@@ -45,7 +64,7 @@ var RouteView = Backbone.View.extend({
     this.$('.toggle').prop('checked', false);
   },
   only: function(e) {
-    this.model.trigger('only');
+    this.model.trigger('only', this);
   }
 });
 
@@ -78,23 +97,33 @@ var RouteMapView = Backbone.View.extend({
 var TransvisorApp = Backbone.View.extend({
   // Overall app View.
   initialize: function(options) {
+    this.route_groups = {};
+    this.routes = new AgencyCollection();
+    this.routes.url = options.url;
     this.leaflet = options.leaflet;
     this.layer = new L.LayerGroup().addTo(this.leaflet); 
-    this.listenTo(Routes, 'add', this.addOne);
-    this.listenTo(Routes, 'sync', this.fitall);
-    this.listenTo(Routes, 'only', this.only);
-    Routes.fetch();
+    this.listenTo(this.routes, 'add', this.addOne);
+    this.listenTo(this.routes, 'sync', this.fitall);
+    this.listenTo(this.routes, 'only', this.only);
+    this.routes.fetch();
   },
   addOne: function(route) {
+    // Add the route to a route group.
+    console.log(route);
+    console.log(route, route.model);
+    console.log(route.get('properties').route_short_name);
+    
     // Add a Route to the view.
     var view = new RouteView({model: route});
-    this.$("#cityism-transvisor-routes").append(view.render().el);
+    this.$(".cityism-transvisor-routes").append(view.render().el);
+    
     // And also add a second View for the map layer.
     var view_map = new RouteMapView({model: route});
     this.layer.addLayer(view_map.render());
   },  
-  only: function(e) {
-    Routes.each(function(i){i.set('display', false)});
+  only: function(route) {
+    this.routes.each(function(i){i.set('display', false)});
+    route.model.set('display', true);
   },
   fitall: function() {
     // Fit all routes within the map.
